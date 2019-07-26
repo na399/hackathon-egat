f3e2a0f0f3147d621719611f662784770f7eb5f7
Max Adulyanukosol <namax7@gmail.com>
2019-07-26 10:35:10 +0700

    Fix live loading bug attempt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,40 @@
-
 export default {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#F6E05E' },
   /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/css/main.css',
-    '~/assets/css/tailwind.css'
-  ],
+   ** Global CSS
+   */
+  css: ['~/assets/css/main.css', '~/assets/css/tailwind.css'],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/pwa',
-    'nuxt-vuex-localstorage'
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa', 'nuxt-vuex-localstorage'],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     postcss: {
       plugins: {
@@ -48,9 +42,13 @@ export default {
       }
     },
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  },
+  generate: {
+    minify: {
+      collapseWhitespace: false
     }
   }
 }
